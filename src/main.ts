5347284b6a0c508a1550288c216cf0da7afb37cf
yozawa4662 <yuyaron01@gmail.com>
2026-01-01 23:02:01 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,94 @@
+import './style.css'
+
+/*
+** DOM
+*/
+const avgDisplay = document.querySelector<HTMLDivElement>('#avg-display')!;
+const bpmDisplay = document.querySelector<HTMLDivElement>('#bpm-display')!;
+const modeSelect = document.querySelector<HTMLSelectElement>('#mode-select')!;
+const peakDisplay = document.querySelector<HTMLSpanElement>('#peak-display')!;
+const resetBtn = document.querySelector<HTMLButtonElement>('#reset-btn')!;
+const samplingSelect = document.querySelector<HTMLSelectElement>('#sampling-select')!;
+
+/*
+** States
+*/
+let lastKeyTime: number | null = null;
+let mode: number = 4;
+let maxBpm = 0;
+let numSamples = 16;
+let recentIntervals: number[] = [];
+let startTime: number | null = null;
+let totalKeyCount = 0;
+
+/*
+** Functions
+*/
+const fixed1 = (num: number) => num.toFixed(1);
+
+const updateDisplay = (currentBpm: number, avgBpm: number) => {
+    avgDisplay.textContent = fixed1(avgBpm);
+    bpmDisplay.textContent = fixed1(currentBpm);
+    peakDisplay.textContent = fixed1(maxBpm);
+}
+
+const reset = () => {
+    lastKeyTime = null;
+    maxBpm = 0;
+    recentIntervals = [];
+    startTime = null;
+    totalKeyCount = 0;
+    updateDisplay(0, 0);
+}
+
+/*
+** Events
+*/
+modeSelect.addEventListener('change', () => {
+    mode = Number(modeSelect.value);
+    reset();
+});
+
+resetBtn.addEventListener('click', reset);
+
+samplingSelect.addEventListener('change', () => {
+    numSamples = Number(samplingSelect.value);
+    if (recentIntervals.length > numSamples) {
+        recentIntervals = recentIntervals.slice(-numSamples);
+    }
+});
+
+window.addEventListener('keydown', (e) => {
+    if (e.repeat) return;
+
+    const now = performance.now();
+    totalKeyCount++;
+
+    if (startTime === null) {
+        startTime = now;
+    }
+
+    // average bpm
+    let overallAvgBpm = 0;
+    if (totalKeyCount > 1 && startTime !== null) {
+        const totalDurationMin = (now - startTime) / 60 / 1000;
+        overallAvgBpm = (totalKeyCount / totalDurationMin) / mode;
+    }
+
+    // current bpm
+    let currentBpm = 0;
+    if (lastKeyTime !== null) {
+        const interval = now - lastKeyTime;
+
+        recentIntervals.push(interval);
+        if (recentIntervals.length > numSamples) recentIntervals.shift();
+        const avgInterval = recentIntervals.reduce((a, b) => a + b) / recentIntervals.length;
+        currentBpm = (60 * 1000 / avgInterval) / mode;
+    }
+
+    // max bpm
+    if (currentBpm > maxBpm) maxBpm = currentBpm;
+
+    updateDisplay(currentBpm, overallAvgBpm);
+    lastKeyTime = now;
+});
